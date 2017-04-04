6babea872aa6045ded7d1ab3d104a6f5bb5fcef9
Liutong Chen <liutongchen@users.noreply.github.com>
2017-04-04 12:05:48 -0700

    Update understanding_function_invocation.js

--- understanding_function_invocation.js
+++ understanding_function_invocation.js
@@ -31,6 +31,8 @@ assert(ninja2.getMyThis()===ninja2, "working on ninja2")
 
 
 //using constructor functions
+
+//1. constructor functions returning the object instance itself
 function ninja() {
   this.skulk = function() {
     return this
@@ -42,3 +44,32 @@ var ninja2 = new ninja();
 
 assert(ninja1.skulk() === ninja1, "ninja1 is skulking");
 assert(ninja2.skulk() === ninja2, "ninja2 is skulking");
+
+//2. constructor functions returning the a primitive: ignore the primitive and return the newly created object
+function Ninja() {
+  this.sulk = function() {
+    return true
+  }
+  return 1
+}
+
+var ninja = new Ninja();
+
+assert(Ninja() === 1, "function invocation as function return 1");
+assert(typeof ninja === "object", "object returned when called as a constructor");
+assert(typeof ninja.skulk === "function", "ninja object has a skulk method");
+
+//3. constructors returning othe object values: object returned by the constructor is assigned to the variable "emperor"
+var puppet = {
+  rule: false
+}
+
+function Emperor() {
+  this.rule = true;
+  return puppet;
+}
+
+var emperor = new Emperor();
+
+assert (emperor === puppet, "this emperor is a puppet")
+assert(emperor.rule === false, "this new emperor doesn't know how to rule")
