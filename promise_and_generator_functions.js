35a8c295bdaf066f31532421e38ccbb843a3fe85
Liutong Chen <liutongchen@users.noreply.github.com>
2017-04-08 20:09:39 -0700

    Update promise_and_generator_functions.js

--- promise_and_generator_functions.js
+++ promise_and_generator_functions.js
@@ -6,15 +6,27 @@ function assert(a, b) {
   }
 }
 
-//simple generator function
-
 function* mealCooker() {
-  yield "Chinese cuisine";
   yield "Japanese cuisine";
+  yield* chineseCuisineGenerator();
   yield "Canadian poutin";
+  
 }
 
+function* chineseCuisineGenerator() {
+  yield "Cantonese cuisine";
+  yield "Yangzhou cuisine";
+  yield "Sichuan cuisine";
+}
+/*
+//version 1.0 of getting each meal (using while loop)
+let mealIterator = mealCooker();
+let meal;
+while (!(meal = mealIterator.next()).done) {
+  assert(meal !== null, meal.value)
+}
+*/
+//version 1.1 of getting each meal (using for-of loop)
 for (let meal of mealCooker()) {
-  assert(meal !== undefined, meal)
+  assert(meal !== null, meal)
 }
-
