366286ec48db1be78122a7a3378db4bdf75a9544
Liutong Chen <liutongchen@users.noreply.github.com>
2017-04-18 22:01:12 -0700

    Update object_orientation_with_prototypes.js

--- object_orientation_with_prototypes.js
+++ object_orientation_with_prototypes.js
@@ -20,3 +20,24 @@ Object.defineProperty(ninja0, "constructor", {
 })
   //before configuring properties
 assert(ninja0.constructor === Ninja, "Ninja is the constructor of ninja0")
+
+
+//2. using class in ES6 to simplify prototype chain----------------------------------------------------
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  
+  dance() {
+    return true;
+  }
+}
+
+class Ninja extends Person {
+  constructor(name) {
+    super(name);
+  }
+}
+
+let ninja2 = new Ninja();
+assert(ninja2.constructor === Ninja, "ninja2's constructor property is kept without configuration") ;
