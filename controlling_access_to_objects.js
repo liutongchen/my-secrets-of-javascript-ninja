d2bd4ac88b8beee0d6646ea764599753727c345f
Liutong Chen <liutongchen@users.noreply.github.com>
2017-04-19 17:01:09 -0700

    Create controlling_access_to_objects.js

---
+++ controlling_access_to_objects.js
@@ -0,0 +1,3 @@
+//1. Two ways to define getters and setters--------------------------------------------------------------------
+//1) using object literals or using ES6 class definitions
+//2) using Object.defineProperty method
