febb502eb009c93c8a11898cbafd36e7cd1ca521
Liutong Chen <liutong.clt@gmail.com>
2017-04-19 20:03:16 -0700

    using closure to set up getters and setters

--- controlling_access_to_objects.js
+++ controlling_access_to_objects.js
@@ -20,4 +20,23 @@ assert(ninjaCollection.firstNinja === "naruto", "The first Ninja is Naruto");
 ninjaCollection.firstNinja = "Sakura";
 assert(ninjaCollection.firstNinja === "Sakura", "The first ninja is now Sakura");
 
-//2) using Object.defineProperty method
+//2) using Object.defineProperty method (closure)
+function Ninja() {
+    let ninjas = ["naruto", "sasuke", "kakashi"];
+    Object.defineProperty(this, "firstNinja", {
+        get: () => {
+            console.log("Getting the first Ninja using closure");
+            return ninjas[0];
+        },
+        set: value => {
+            console.log("Setting the first Ninja using closure");
+            ninjas[0] = value;
+        }
+    });
+}
+
+let ninjaCollection = new Ninja();
+assert(ninjaCollection.ninjas === "undefined", "Don't have direct access to ninjas property");
+assert(ninjaCollection.firstNinja === "naruto", "The first ninja is Naruto");
+ninjaCollection.firstNinja = "Sakura";
+assert(ninjaCollection.firstNinja === "Sakura", "The first ninja is now Sakura");
