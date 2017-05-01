99d62f173091185199567d22c0c5d4d070a0f016
Liutong Chen <liutongchen@users.noreply.github.com>
2017-05-01 13:05:52 -0700

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,3 @@
+//1. Removing and adding items at arbitrary positions of an array (note that "delete" method cannot ideally achieve this)-----
+let ninja1 = ["naruto", "kakashi", "jilaiye"];
+ninja1.splice(2, 1, "Sakura", "Sasuke")
