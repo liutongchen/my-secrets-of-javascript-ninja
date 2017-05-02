b8dcda16e3d1d6d8fbfe92d6371ed21d3d48e224
Liutong Chen <liutongchen@users.noreply.github.com>
2017-05-01 23:19:44 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,20 @@
 //1. Removing and adding items at arbitrary positions of an array (note that "delete" method cannot ideally achieve this)-----
 let ninja1 = ["naruto", "kakashi", "jilaiye"];
 ninja1.splice(2, 1, "Sakura", "Sasuke")
+
+//2. Testing arrays with "every" and "some" methods----------------------------------------------------------------------------
+function assert(a, b) {
+  if (a) { console.log(b); }
+  else { console.log("Oops"); }
+}
+let ninjas = [{name: "naruto", food: "ramen"}, 
+{name: "kakashi", food: "ramen"}, 
+{name: "jilaiye"}];
+
+let allNinjasAreNamed = ninjas.every(ninja => ninja.name);
+let allNinjasLikeFood = ninjas.every(ninja => ninja.food);
+let someNinjasLikeFood = ninjas.some(ninja => ninja.food);
+
+assert(allNinjasAreNamed, "All ninjas are named");
+assert(!allNinjasLikeFood, "Not all ninjas like food");
+assert(someNinjasLikeFood, "But some ninjas like food");
