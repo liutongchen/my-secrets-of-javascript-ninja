80dd48ae28560bf39f75aa9a1bd7a75ad5db1731
Liutong Chen <liutongchen@users.noreply.github.com>
2017-05-02 18:20:00 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,12 +1,13 @@
+function assert(a, b) {
+  if (a) { console.log(b); }
+  else { console.log("Oops"); }
+}
+
 //1. Removing and adding items at arbitrary positions of an array (note that "delete" method cannot ideally achieve this)-----
 let ninja1 = ["naruto", "kakashi", "jilaiye"];
 ninja1.splice(2, 1, "Sakura", "Sasuke")
 
 //2. Testing arrays with "every" and "some" methods----------------------------------------------------------------------------
-function assert(a, b) {
-  if (a) { console.log(b); }
-  else { console.log("Oops"); }
-}
 let ninjas = [{name: "naruto", food: "ramen"}, 
 {name: "kakashi", food: "ramen"}, 
 {name: "jilaiye"}];
@@ -32,3 +33,23 @@ const elems = {
     Array.prototype.find.call(this, callback);
   }
 }
+
+//4.create map------------------------------------------------------------------------------------------------------------------
+let ninjaIslandMap = new Map();
+let ninja1 = {name: "Naruto"};
+let ninja2 = {name: "Kakashi"};
+let ninja3 = {name: "Jilaiye"};
+//set method
+ninjaIslandMap.set(ninja1, {food: "ramen"});
+ninjaIslandMap.set(ninja2, {food: "Putin"});
+//get method
+assert(ninjaIslandMap.get(ninja1).food === "ramen", "Ninja1 likes ramen")
+assert(ninja1.name === "Naruto", "we can track the name of Ninja1");
+//size property
+assert(ninjaIslandMap.size === 2, "there are two ninjas on ninja island")
+//has method
+assert(ninjaIslandMap.has(ninja2), "ninja2 is also on the island");
+//delete and clear method
+assert(ninjaIslandMap.delete(ninja2), "ninja2 is gone");
+ninjaIslandMap.clear()
+assert(ninjaIslandMap.size === 0, "all ninjas are gone now")
