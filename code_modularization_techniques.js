bc40a5ba74d4324848db0f097f96cf90fd23ef75
Liutong Chen <liutongchen@users.noreply.github.com>
2017-05-04 16:55:52 -0700

    Update code_modularization_techniques.js

--- code_modularization_techniques.js
+++ code_modularization_techniques.js
@@ -14,5 +14,25 @@ const countClickModule = function() {
 
 countClickModule.clickCounter();
 
+//task2: augment the above module without modifying the original code
+(function(module) {
+  let counter = 0;
+  let scrollHandler = () => {
+    console.log(scrollCounter++);
+  };
+  module.scrollCounter = () => 
+    document.addEventListener("wheel", scrollHandler);
+})(countModule);
 
+//2. using AMD to specify a module dependent on jQuery------------------------------------------------------------------------------------
+define("counterModule", ["jQuery"], 
+      $ => {
+          let num = 0;
+          let handleClick = () => {console.log(num++);};
+          return {
+            clickCounter: () => $(document).on("click", handleClick)
+          }
+      }
+)
 
+//3. using CommonJS to define a module
