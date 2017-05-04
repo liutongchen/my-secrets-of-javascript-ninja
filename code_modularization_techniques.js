e7a7e1bdbac92a1e64ad6794e1b50491514c2b10
Liutong Chen <liutongchen@users.noreply.github.com>
2017-05-04 14:43:57 -0700

    Update code_modularization_techniques.js

--- code_modularization_techniques.js
+++ code_modularization_techniques.js
@@ -1,1 +1,18 @@
+//1. using objects, closures and immediate functions to specify modules---------------------------------------------------------------
+//task1: count clicks on a web page
+const countClickModule = function() {
+  let counter = 0;
+  let handleClick = function() {
+    alert(counter++)
+  };
+  return {
+    clickCounter: function() {
+      document.addEventListener("click", handleClick)
+    }
+  }
+}();
+
+countClickModule.clickCounter();
+
+
 
